2dae04f973fad00a72e40d35dc8a9242fff378fb
Shinya Uryu <suika1127@gmail.com>
2016-01-28 18:52:14 +0900

    upgrade sync

--- R/wt_sync.R
+++ R/wt_sync.R
@@ -1,86 +1,61 @@
-#' Sync file modify data as heartbeat
-#' @importFrom httr add_headers
-#' @importFrom httr POST
+#' Sync file modify data to local database
+#' @import dplyr
 #' @name wt_sync
 wt_sync <- function() {
-  .wakatimerEnv$post.token <- write_scope()
-  httr::POST(
-    "https://api.wakatime.com/",
-    path = "api/v1/heartbeats",
-    config = httr::add_headers(
-      `User-Agent` = paste0(
-        sessionInfo()$R.version$language,
-        "/",
-        sessionInfo()$R.version$major,
-        ".",
-        sessionInfo()$R.version$minor,
-        " RStudio-",
-        rstudioapi::versionInfo()$mode,
-        "/",
-        rstudioapi::getVersion(),
-        " wakatimer/",
-        packageVersion("wakatimer")
-      ),
-      `connection` = "keep-alive",
-      `accept-encoding` = "gzip,deflate",
-      `Authorization` = paste(
-        "Bearer",
-        .wakatimerEnv$post.token$credentials$access_token
-      ),
-      `Content-Type`  = "application/json"
-    ),
-    body = file_info(),
-    encode = "json"
-  )
-}
+  db_con <-
+    dplyr::src_sqlite(paste(path.package("wakatimer"), ".wakatime.db", sep = "/"), create = FALSE)
 
-#' Sync modificate files between sesion
-#' @import dplyr
-#' @importFrom jsonlite toJSON
-#' @importFrom rlist list.load
-#' @name wt_sync_session
-wt_sync_session <- function() {
-  if (is.null(.wakatimerEnv$post.token))
-    .wakatimerEnv$post.token <- write_scope()
+  df.files.load <- db_con %>%
+    dplyr::tbl(ifelse(is.null(get_rproj_name()), "heartbeats_1", get_rproj_name())) %>%
+    as.data.frame() %>%
+    dplyr::rename(entity = file) %>%
+    dplyr::filter(grepl(
+      paste0(rstudioapi::getActiveProject(), "/.+"),
+      path.expand(entity)
+    )) %>%
+    dplyr::rowwise() %>%
+    dplyr::mutate(time = as.numeric(mtime)) %>%
+    dplyr::ungroup() %>%
+    dplyr::select(-size,-mtime)
 
-  # .wakatimerEnv$df.files <- recent_files()
-  .wakatimerEnv$df.files.load <-
-    record_files(df = .wakatimerEnv$df.files) %>%
-    dplyr::mutate(is_write = FALSE)
-
-  .wakatimerEnv$df.files.quit <-
+  df.files.quit <-
     recent_files() %>% record_files() %>%
-    dplyr::bind_rows(.wakatimerEnv$df.files.load) %>%
+    dplyr::bind_rows(df.files.load) %>%
     dplyr::distinct(time)
 
-  .wakatimerEnv$exist.files <-
-    .wakatimerEnv$df.files.quit %>% dplyr::filter(is_write == FALSE) %>% .$entity
+  exist.files <-
+    df.files.quit %>% dplyr::filter(is_write == 0) %>% .$entity
 
-  .wakatimerEnv$df.files.diff <- .wakatimerEnv$df.files.quit %>%
-    dplyr::filter(entity %in% .wakatimerEnv$exist.files) %>%
-    dplyr::arrange(time, entity) %>%
-    dplyr::group_by(entity) %>%
-    dplyr::summarise(status = lines[2] - lines[1]) %>%
-    dplyr::mutate(status = ifelse((status == 0), status,
-                                  ifelse((status > 1), paste0("+", status), status)))
+  df.sync <- df.files.quit %>%
+    dplyr::filter(entity %in% exist.files)
 
-  json.sync <- .wakatimerEnv$df.files.quit %>%
-    dplyr::filter(entity %in% .wakatimerEnv$exist.files) %>%
-    dplyr::arrange(time, entity) %>%
-    dplyr::mutate(
-      type = "file",
-      lineno = NULL,
-      cursorpos = NULL,
-      project = get_rproj_name(),
-      branch = get_hbranch_name(),
-      language = gsub(".+\\.", "", entity),
-      is_debugging = FALSE,
-      is_write = ifelse(is.na(is_write), TRUE, FALSE)
-    ) %>%
+  if (nrow(df.sync) >= 1) {
+    df.sync %<>% dplyr::arrange(time, entity) %>%
+      dplyr::mutate(
+        type = "file",
+        lineno = NULL,
+        cursorpos = NULL,
+        project = ifelse(is.null(get_rproj_name()), "", get_rproj_name()),
+        branch = ifelse(is.null(get_hbranch_name()), "", get_hbranch_name()),
+        language = gsub(".+\\.", "", entity),
+        is_debugging = FALSE,
+        is_write = ifelse(is.na(is_write), TRUE, FALSE)
+      )
+  }
+}
+
+#' Sync file modify data as heartbeats
+#' @importFrom jsonlite toJSON
+#' @importFrom rlist list.load
+#' @importFrom httr POST
+#' @importFrom httr add_headers
+#' @importFrom pforeach npforeach
+#' @name wt_post
+wt_post <- function() {
+  json.sync <- wt_sync() %>%
     jsonlite::toJSON(pretty = FALSE) %>%
     rlist::list.load()
 
-  if (is.list(json.sync) & length(json.sync) >= 1)
   if (is.list(json.sync) & length(json.sync) >= 1) {
     pforeach::npforeach(i = 1:length(json.sync))({
       httr::POST(
@@ -104,7 +79,7 @@ wt_sync_session <- function() {
           `accept-encoding` = "gzip,deflate",
           `Authorization` = paste(
             "Bearer",
-            .wakatimerEnv$post.token$credentials$access_token
+            gen_token() %>% .$credentials %>% .$access_token
           ),
           `Content-Type`  = "application/json"
         ),
@@ -112,4 +87,57 @@ wt_sync_session <- function() {
         encode = "json"
       )
     })
+    .wakatimerEnv$db_con <-
+      dplyr::src_sqlite(paste(path.package("wakatimer"), ".wakatime.db", sep = "/"), create = FALSE)
+    dplyr::db_drop_table(.wakatimerEnv$db_con$con,
+                         ifelse(is.null(get_rproj_name()), "heartbeats_1", get_rproj_name()))
+  }
+}
+
+#' Sync modificate files between sesion
+#' @import dplyr
+#' @importFrom jsonlite toJSON
+#' @importFrom rlist list.load
+#' @name wt_sync_session
+wt_sync_session <- function() {
+  .wakatimerEnv$db_con <-
+    dplyr::src_sqlite(paste(path.package("wakatimer"), ".wakatime.db", sep = "/"), create = FALSE)
+
+  if (dplyr::db_has_table(.wakatimerEnv$db_con$con,
+                          ifelse(is.null(get_rproj_name()), "heartbeats_1", get_rproj_name()))) {
+    wt_post()
+  } else {
+    dplyr::copy_to(
+      dest = .wakatimerEnv$db_con,
+      df   = recent_files() %>%
+        dplyr::mutate(is_write = 0),
+      name = ifelse(
+        is.null(get_rproj_name()),
+        "heartbeats_1",
+        get_rproj_name()
+      ),
+      temporary = FALSE
+    )
+  }
+}
+
+#' Sync local database
+#' @import dplyr
+#' @name wt_sync_local
+wt_sync_local <- function() {
+  .wakatimerEnv$db_con <-
+    dplyr::src_sqlite(paste(path.package("wakatimer"), ".wakatime.db", sep = "/"), create = FALSE)
+  if (dplyr::db_has_table(.wakatimerEnv$db_con$con,
+                          ifelse(is.null(get_rproj_name()), "heartbeats_1", get_rproj_name())))
+    dplyr::db_drop_table(.wakatimerEnv$db_con$con,
+                         ifelse(is.null(get_rproj_name()), "heartbeats_1", get_rproj_name()))
+
+  dplyr::copy_to(
+    dest = .wakatimerEnv$db_con,
+    df   = wt_sync(),
+    name = ifelse(is.null(get_rproj_name()),
+                  "heartbeats_1",
+                  get_rproj_name()),
+    temporary = FALSE
+  )
 }
