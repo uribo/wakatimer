17af72b0b820de65b6d0e9f28a3fd731b4e69546
Shinya Uryu <suika1127@gmail.com>
2016-01-28 18:53:19 +0900

    misc.

--- R/utilities.R
+++ R/utilities.R
@@ -4,7 +4,6 @@
 # - check_file_active()
 # - get_rproj_name()
 # - get_hbranch_name()
-# - file_info()
 # - record_files()
 
 #' Platform versioning
@@ -84,31 +83,16 @@ recent_files <- function(n = 15) {
     dplyr::select(file, size, mtime) %>%
     dplyr::filter(!is.na(size)) %>%
     dplyr::rowwise() %>%
-    dplyr::mutate(lines = length(readLines(file)))
+    dplyr::mutate(lines = length(readLines(file))) %>%
+    dplyr::ungroup()
 
     return(.wakatimerEnv$df.files)
-}
-
-
-#' Check open file condition
-#' @importFrom rstudioapi getActiveDocumentContext
-#' @name check_file_active
-check_file_active <- function() {
-  if (!rstudioapi::getActiveDocumentContext()$path %in% .wakatimerEnv$df.files$file) {
-    .wakatimerEnv$df.files <- recent_files()
-    if (rstudioapi::getActiveDocumentContext()$path %in% .wakatimerEnv$df.files$file) {
-      rstudioapi::getActiveDocumentContext()$path
-    } else {
-      message("This file is save yet!")
-    }
-  } else {
-    rstudioapi::getActiveDocumentContext()$path
-  }
 }
 
 #' Getting Rproject name
 #' @return Active RProject name
 #' @name get_rproj_name
+#' @export
 get_rproj_name <- function() {
   if (is.null(rstudioapi::getActiveProject())) {
     NULL
@@ -124,6 +108,7 @@ get_rproj_name <- function() {
 #' @import git2r
 #' @importFrom  purrr map_lgl
 #' @name get_hbranch_name
+#' @export
 get_hbranch_name <- function() {
   if (file.exists("./.git") == FALSE) {
     NULL
@@ -135,77 +120,29 @@ get_hbranch_name <- function() {
     head_branch <-
       repo_branches %>%
       purrr::map_lgl(git2r::is_head) %>%
-      which(. == TRUE) %>%
+      which(. == 1) %>%
       as.numeric()
     repo_branches[[head_branch]]@name
   }
 }
 
-#' Making posting data
-#' @import dplyr
-#' @name file_info
-#' @export
-file_info <- function() {
-  active.file <- check_file_active()
-  # FLAG:
-  if (is.null(active.file))
-    warning("This function is abailable on saved source file.", call. = FALSE)
-
-  # FLAG: file modified?
-  if (as.numeric(file.info(active.file, extra_cols = FALSE)$mtime) >
-      .wakatimerEnv$df.files %>%
-      dplyr::filter(file %in% active.file) %>% .$mtime %>% as.numeric()) {
-    flag2save <- TRUE
-  } else {
-    flag2save <- FALSE
-  }
-
-  edit.file.info <- list(
-    entity = active.file,
-    type = "file",
-    time = as.numeric(file.info(active.file, extra_cols = FALSE)$mtime),
-    lines = length(readLines(active.file)),
-    lineno = NULL,
-    cursorpos = NULL,
-    project = NULL,
-    branch = NULL,
-    language =  gsub(".+\\.", "", active.file),
-    is_debugging = FALSE,
-    is_write = flag2save
-  )
-
-  # FLAG: exit in project dir?
-  if (path.expand(active.file) %in% list.files(rstudioapi::getActiveProject(),
-                                               full.names = TRUE,
-                                               recursive = TRUE)) {
-    edit.file.info$project = get_rproj_name()
-    edit.file.info$branch  = get_hbranch_name()
-  }
-
-  # FLAG: Identical edit file and active file
-  if (identical(active.file,
-                rstudioapi::getActiveDocumentContext()$path)) {
-    edit.file.info$lineno = as.numeric(getActiveDocumentContext()$selection[[1]]$range$start["row"])
-    edit.file.info$cursorpos = as.numeric(getActiveDocumentContext()$selection[[1]]$range$start["column"])
-  }
-  return(edit.file.info)
-}
-
 #' Record file status to environment
 #' @param df data frame
 #' @name record_files
+#' @export
 record_files <- function(df = NULL) {
   if (is.null(.wakatimerEnv$df.files))
-    wakatimerEnv$df.files <- recent_files()
-  df %>%
+    recent_files()
+
+  .wakatimerEnv$df.files %>%
     dplyr::rename(entity = file) %>%
     dplyr::filter(grepl(
       paste0(rstudioapi::getActiveProject(), "/.+"),
       path.expand(entity)
     )) %>%
     dplyr::rowwise() %>%
-    dplyr::mutate(
-      time = as.numeric(mtime)) %>%
+    dplyr::mutate(time = as.numeric(mtime)) %>%
+    dplyr::ungroup() %>%
     dplyr::select(-size, -mtime) %>%
     return(.)
 }
